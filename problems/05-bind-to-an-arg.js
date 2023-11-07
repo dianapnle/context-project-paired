8a0ef1b856c926f3d0aa026d1bb8ac0d6f8efb06
ahmed_rajab <coding.one82@gmail.com>
2023-11-07 16:33:54 -0700

    up to 05 solved

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,6 +1,9 @@
 function bindToAnArg(func, arg) {
-  // Your code here 
-}
+  return function(){
+    return func(arg);
+    }
+  }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
